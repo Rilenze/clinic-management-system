6453a6925e2b01232ee305657702f994c07c07d5
Rijad Burović <rburovic1@etf.unsa.ba>
2024-04-17 20:42:50 +0200

    Doctor model created

---
+++ clinic-management-system/Models/Doctor.cs
@@ -0,0 +1,16 @@
+﻿namespace clinic_management_system.Models
+{
+    public enum Title {
+        specialist,
+        resident,
+        nurse
+    }
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public Title title { get; set; }
+        public string code { get; set; }
+    }
+}
