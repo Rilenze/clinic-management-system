dbfe761f666b21bf7eb06e91816d1e39886e8d3d
Rijad Burović <rburovic1@etf.unsa.ba>
2024-04-18 15:26:08 +0200

    Connected to the SqlServer and migrations added

---
+++ clinic-management-system/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using clinic_management_system.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace clinic_management_system.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {    
+        }
+        public DbSet<Doctor> Doctors { get; set; }
+
+    }
+}
