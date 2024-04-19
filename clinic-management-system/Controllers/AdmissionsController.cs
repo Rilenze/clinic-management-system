ede598b6480421fa45f4b92a403717c24ad5198e
Rijad Burović <rburovic1@etf.unsa.ba>
2024-04-19 14:26:26 +0200

    Admissions controller and views with crud operations implemented

---
+++ clinic-management-system/Controllers/AdmissionsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using clinic_management_system.Data;
+using clinic_management_system.Models;
+
+namespace clinic_management_system.Controllers
+{
+    public class AdmissionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdmissionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admissions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Admissions.ToListAsync());
+        }
+
+        // GET: Admissions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var admission = await _context.Admissions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (admission == null)
+            {
+                return NotFound();
+            }
+
+            return View(admission);
+        }
+
+        // GET: Admissions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admissions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AdmissionDateTime,PatientId,DoctorId,Urgency")] Admission admission)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(admission);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(admission);
+        }
+
+        // GET: Admissions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var admission = await _context.Admissions.FindAsync(id);
+            if (admission == null)
+            {
+                return NotFound();
+            }
+            return View(admission);
+        }
+
+        // POST: Admissions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AdmissionDateTime,PatientId,DoctorId,Urgency")] Admission admission)
+        {
+            if (id != admission.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(admission);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdmissionExists(admission.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(admission);
+        }
+
+        // GET: Admissions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var admission = await _context.Admissions
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (admission == null)
+            {
+                return NotFound();
+            }
+
+            return View(admission);
+        }
+
+        // POST: Admissions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var admission = await _context.Admissions.FindAsync(id);
+            if (admission != null)
+            {
+                _context.Admissions.Remove(admission);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AdmissionExists(int id)
+        {
+            return _context.Admissions.Any(e => e.Id == id);
+        }
+    }
+}
