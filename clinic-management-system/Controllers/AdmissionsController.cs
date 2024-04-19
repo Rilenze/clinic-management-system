dcf7016b3550be7ebe032de940145866acec68bd
Rijad Burović <rburovic1@etf.unsa.ba>
2024-04-19 16:58:28 +0200

    Sending ajax call to get doctors from database

--- clinic-management-system/Controllers/AdmissionsController.cs
+++ clinic-management-system/Controllers/AdmissionsController.cs
@@ -49,6 +49,13 @@ namespace clinic_management_system.Controllers
             return View();
         }
 
+        //GET: Admissions/Fetch
+        public IActionResult Fetch()
+        {
+            var doctors = _context.Doctors.ToList();
+            return Json(doctors);
+        }
+
         // POST: Admissions/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
---
+++ clinic-management-system/wwwroot/js/ajaxCall.js
@@ -0,0 +1,21 @@
+﻿console.log("connected");
+
+const url = "https://localhost:44332";
+
+async function fetchData() {
+
+    try {
+        let response = await fetch(`${url}/Admissions/Fetch`);
+
+        let list = await response.json();
+        console.log(list);  
+        
+    } catch (error) {
+        console.error("Failed to fetch data: ", error);
+    }
+}
+
+fetchData();
+
+
+
